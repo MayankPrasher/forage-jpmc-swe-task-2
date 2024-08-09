4da61d11f6a61d2140c300d4c364a1dd3395e9cc
Mayank Prasher <prasher6789@gmail.com>
2024-08-09 21:02:36 +0530

    task completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph:boolean,  //added the showGraph property
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph:false,//initially set the showGraph property to false which will set to be true by the component later
     };
   }
 
@@ -29,18 +31,32 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    //setting the condition to ensure that the graph doesn't render until a user clicks the 'Start Streaming' button
+    if(this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    //modifying getSataFromServer() to contact server and get data from it continously
+    let x = 0;
+    const interval = setInterval(()=>{
+        DataStreamer.getData((serverResponds: ServerRespond[]) => {
+              // Update the state by creating a new array of data that consists of
+              // Previous data in the state and the new data from server
+              this.setState({ 
+                data: serverResponds,
+                showGraph:true,
+               });
+            });
+            x++;
+            if(x>1000){
+              clearInterval(interval);
+            }
+    },100);
   }
 
   /**
